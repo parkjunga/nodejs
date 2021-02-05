fe27d3855eb53745e64d54d8c9d684dd752a5363
parkjungah <asdfg98712@gmail.com>
2021-02-05 09:18:29 +0900

    Node.js 26 App 제작-함수를 이용해서 정리정돈하기

--- main.js
+++ main.js
@@ -6,73 +6,76 @@ var app = http.createServer(function(request,response){
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname;
 
+    function templateHtml(title,list,body){
+      return `
+      <!doctype html>
+      <html>
+      <head>
+        <title>WEB1 - ${title}</title>
+        <meta charset="utf-8">
+      </head>
+      <body>
+        <h1><a href="/">WEB</a></h1>
+        ${list}
+        ${body}
+      </body>
+      </html>
+      `;
+    }
+
+    function templateList(filelist) {
+      var list =  '<ul>';
+      var i = 0;
+      while (filelist.length > i) {
+        list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+        i = i + 1;
+      }
+      list = list + '</ul>';
+      return list;
+    }
+
+
     if (pathname === '/') {
-      if (queryData.id === undefined) {
-          fs.readdir('./data',function(error, filelist){
-            console.log(filelist);
-            var title = 'Welcome';
-            var description = 'Hello.Node.js';
-            var list =  '<ul>';
-            var i = 0;
-            while (filelist.length > i) {
-              list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-              i = i + 1;
-            }
-            list = list + '</ul>';
-            var template = `
-            <!doctype html>
-            <html>
-            <head>
-              <title>WEB1 - ${title}</title>
-              <meta charset="utf-8">
-            </head>
-            <body>
-              <h1><a href="/">WEB</a></h1>
-              ${list}
-              <h2>${title}</h2>
-              <p>${description}</p>
-            </body>
-            </html>
-            `;
-            response.writeHead(200);
-            response.end(template);
-          })
-      } else {
-        fs.readdir('./data',function(error, filelist){
-          console.log(filelist);
-          var title = 'Welcome';
-          var description = 'Hello.Node.js';
-          var list =  '<ul>';
-          var i = 0;
-          while (filelist.length > i) {
-            list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-            i = i + 1;
-          }
-          list = list + '</ul>';
+      fs.readdir('./data',function(error, filelist){
+        const list = templateList(filelist)
+        if (queryData.id === undefined) {
+          const title = 'Welcome';
+          const description = 'Hello.Node.js';
+          const template = templateHtml(title,list,`<h2>${title}</h2>${description}`);
+          response.writeHead(200);
+          response.end(template);
+        } else {
           fs.readFile(`data/${queryData.id}`,'utf8',function(err,description){
-            var title = queryData.id;
-            var template = `
-            <!doctype html>
-            <html>
-            <head>
-              <title>WEB1 - ${title}</title>
-              <meta charset="utf-8">
-            </head>
-            <body>
-              <h1><a href="/">WEB</a></h1>
-              ${list}
-              <h2>${title}</h2>
-              <p>${description}</p>
-            </body>
-            </html>
-            `;
+            const title = queryData.id;
+            const template = templateHtml(title,list,`<h2>${title}</h2>${description}`);
             response.writeHead(200);
             response.end(template);
           }) ;
-        
-        });
+        }
 
-      }
+      })
+
+      // if (queryData.id === undefined) {
+      //     fs.readdir('./data',function(error, filelist){
+      //       var title = 'Welcome';
+      //       var description = 'Hello.Node.js';
+      //       var list = templateList(filelist)
+      //       var template = templateHtml(title,list,`<h2>${title}</h2>${description}`);
+      //       response.writeHead(200);
+      //       response.end(template);
+      //     })
+      // } else {
+      //   fs.readdir('./data',function(error, filelist){
+      //     fs.readFile(`data/${queryData.id}`,'utf8',function(err,description){
+      //       var title = queryData.id;
+      //       var template = templateHtml(title,list,`<h2>${title}</h2>${description}`);
+      //       response.writeHead(200);
+      //       response.end(template);
+      //     }) ;
+        
+      //   });
+
+      // }
 
     } else {
       response.writeHead(404);
