5bc080bc69f13d263422a34c240d17013d419f42
parkjungah <asdfg98712@gmail.com>
2021-02-05 12:05:26 +0900

    Node.js ~ 37 수정,삭제 기능구현

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ var fs = require('fs');
 var url = require('url'); // node.js가 갖고 있는 모듈중 url모듈을 사용할 것이다.
 var qs = require('querystring');
 
-function templateHtml(title,list,body){
+
+
+function templateHtml(title,list,body, control){
   return `
   <!doctype html>
   <html>
@@ -14,7 +16,7 @@ function templateHtml(title,list,body){
   <body>
     <h1><a href="/">WEB</a></h1>
     ${list}
-    <a href="/create">글 작성하기</a>
+    ${control}
     ${body}
   </body>
   </html>
@@ -47,13 +49,21 @@ var app = http.createServer(function(request,response){
         if (queryData.id === undefined) {
           const title = 'Welcome';
           const description = 'Hello.Node.js';
-          const template = templateHtml(title,list,`<h2>${title}</h2>${description}`);
+          const template = templateHtml(title,list,`<h2>${title}</h2>${description}`,
+          `<a href="/create">글 작성하기</a>
+          `);
           response.writeHead(200);
           response.end(template);
         } else {
           fs.readFile(`data/${queryData.id}`,'utf8',function(err,description){
             const title = queryData.id;
-            const template = templateHtml(title,list,`<h2>${title}</h2>${description}`);
+            const template = templateHtml(title,list,`<h2>${title}</h2>${description}`,
+            `<a href="/create">작성하기</a>
+            <a href="/update?id=${title}">수정하기</a>
+            <form action="delete_process" method="post">
+            <input type="hidden" name="id" value="${title}"/>
+            <input type="submit" value="delete" />
+            </form>`);
             response.writeHead(200);
             response.end(template);
           }) ;
@@ -88,7 +98,7 @@ var app = http.createServer(function(request,response){
           var title = 'Web -- create';
           var list = templateList(filelist)
           var template = templateHtml(title,list,`
-          <form action="http://localhost:3000/create_process" method="post">
+          <form action="/create_process" method="post">
           <p><input type="text" name="title" placeholder="title"></p>
           <p>
           <textarea name="description" placeholder="description"></textarea>
@@ -97,11 +107,11 @@ var app = http.createServer(function(request,response){
           <input type="submit">
           </p>
           </form>
-          `);
+          `,``);
           response.writeHead(200);
           response.end(template);
       })
-    } else if( pathname === '/create_process') {
+    } else if(pathname === '/create_process') {
       var body = "";
       request.on('data', function(data) {
         body = body + data; // callback 데이터를 추가해준다.
@@ -110,12 +120,73 @@ var app = http.createServer(function(request,response){
       request.on('end', function(){
         var post = qs.parse(body); // 지금까지 저장한 body값을 주면
         var title = post.title;
-        var descript = post.description;
+        var description = post.description;
         console.log(post.title);
+        fs.writeFile(`data/${title}`, description,'utf8', function(){
+          response.writeHead(302, {Location: `/?id=${title}`}); // 페이지 리다이렉트
+          response.end();
+        })
       });
       
-      response.writeHead(200);
-      response.end('success');
+
+    } else if(pathname === '/update') {
+      fs.readdir('./data',function(error, filelist){
+            fs.readFile(`data/${queryData.id}`,'utf8',function(err,description){
+              const title = queryData.id;
+              var list = templateList(filelist)
+              const template = templateHtml(title,list,
+              `
+              <form action="/update_process" method="post">
+              <input type="hidden" name="id" value=${title}/>
+              <p><input type="text" name="title" placeholder="title" value=${title}></p>
+              <p>
+              <textarea name="description" placeholder="description">${description}</textarea>
+              </p>
+              <p>
+              <input type="submit">
+              </p>
+              </form> 
+              `,
+              `<a href="/create">글 작성하기</a>
+              <a href="/update?id=${title}">글 수정하기</a>`);
+              response.writeHead(200);
+              response.end(template);
+            }) ;
+      })
+    } else if (pathname === '/update_process') {
+      var body = '';
+      request.on('data', function(data){
+        body =  body + data;
+      })
+      request.on('end',function(){
+        var post = qs.parse(body);
+        var id = post.id;
+        var title = post.title;
+        var description = post.description;
+        console.log(post);
+
+        fs.rename(`data/${id}`,`data/${title}`, function(error){
+          fs.writeFile(`data/${title}`, description, 'utf8', function(){
+            response.writeHead(302, {Location:`/?id=${title}`});
+            response.end();
+          })
+        })
+      })
+
+    } else if (pathname === '/delete_process') {
+      var body = '';
+      request.on('data', function(data){
+        body += data;
+      });
+      request.on('end', function(){
+        var post = qs.parse(body);
+        var id = post.id;
+        fs.unlink(`data/${id}`,function(error){
+          response.writeHead(302, {Location:`/`});
+          response.end();
+        })
+      })
+
     } else {
       response.writeHead(404);
       response.end('Not Found')
