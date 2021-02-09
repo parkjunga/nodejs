130b58a5e18d6318c1b3fa313e8849e1fd87afad
parkjungah <asdfg98712@gmail.com>
2021-02-09 12:37:21 +0900

    Node.js & Mysql 저자 등록,수정,삭제 기능 구현

--- main.js
+++ main.js
@@ -5,13 +5,12 @@ var qs = require('querystring');
 var template = require('./lib/template.js');
 var db = require('./lib/db');
 var topic = require('./lib/topic');
+var author = require('./lib/author');
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname;
-
-
 
     if (pathname === '/') {
         if (queryData.id === undefined) {
@@ -35,6 +34,18 @@ var app = http.createServer(function(request,response){
     } else if (pathname === '/delete_process') {
       topic.delete_process(request,response);
 
+    } else if (pathname === '/author') {
+
+      author.home(request,response);
+    } else if (pathname === '/author/create_process') {
+
+      author.create_process(request, response);
+    } else if (pathname === '/author/update') {
+      author.update(request,response);
+    } else if (pathname === '/author/update_process') {
+      author.update_process(request,response);
+    } else if (pathname === '/author/delete_process') {
+      author.delete_process(request,response);
     } else {
       response.writeHead(404);
       response.end('Not Found')
